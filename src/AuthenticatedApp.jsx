f05358eb9649e1b6ecb1acc9d4314d0fc897792f
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-11-26 19:10:01 -0800

    Make Dashboard page the root route

--- src/AuthenticatedApp.jsx
+++ src/AuthenticatedApp.jsx
@@ -23,11 +23,11 @@ function AppRoutes() {
 	return (
 		<div className="md:ml-52 mt-20 md:mt-0 mb-24">
 			<Routes>
-				<Route path="/" element={<ViewInventory />} />
+				<Route path="/" element={<Dashboard />} />
+				<Route path="/dashboard" element={<Dashboard />} />
 				<Route path="/view-inventory" element={<ViewInventory />} />
 				<Route path="/count-inventory" element={<CountInventory />} />
 				<Route path="/receive-inventory" element={<ReceiveInventory />} />
-				<Route path="/dashboard" element={<Dashboard />} />
 				<Route path="/product/:id" element={<Product />} />
 			</Routes>
 		</div>
