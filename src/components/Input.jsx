429060bd6b2b588a9f8bc34b89508006301bb6c0
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-11-28 13:51:42 -0800

    Create input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Input = ({
+	label = null,
+	sublabel = null,
+	type = "text",
+	className = "",
+	...props
+} = {}) => {
+	return (
+		<div className="form-control w-full">
+			{label ? (
+				<label className="label">
+					<span className="label-text">{label}</span>
+				</label>
+			) : null}
+			<input
+				type={type}
+				className="input input-bordered w-full"
+				{...props}
+			/>
+			{sublabel ? (
+				<label className="label">
+					<span className="label-text-alt">{sublabel}</span>
+				</label>
+			) : null}
+		</div>
+	);
+};
+
+export { Input };
