051855e64e56f56032c8c905e454e08332c02228
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-11-28 22:50:42 -0800

    Use ModalIcon component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useContext } from "react";
+import { CloseIcon } from "./icons";
 
 const callAll =
 	(...fns) =>
@@ -23,19 +24,7 @@ const ModalDismissButton = ({ onClick: secondaryOnClick }) => {
 	return (
 		<div className="absolute right-7">
 			<button onClick={onClick}>
-				<svg
-					height="24"
-					width="24"
-					clipRule="evenodd"
-					fillRule="evenodd"
-					strokeLinejoin="round"
-					strokeMiterlimit="2"
-					viewBox="0 0 24 24"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="currentColor"
-				>
-					<path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z" />
-				</svg>
+				<CloseIcon />
 			</button>
 		</div>
 	);
