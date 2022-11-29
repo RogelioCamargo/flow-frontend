6b544dab4891c1903dd785d9b910eb2b58a3e865
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-11-28 22:52:50 -0800

    Implement search feature and sort products by name in ViewInventory page

--- src/pages/ViewInventory.jsx
+++ src/pages/ViewInventory.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useCreateProduct } from "../utils/products";
 import { useCategories } from "../utils/categories";
 import { useProducts } from "../utils/products";
-import formatDate from "../utils/formatter";
+import { formatDate } from "../utils/formatter";
 import { ProductName, ProductTable } from "../components/ProductTable";
 import {
 	Modal,
@@ -11,8 +11,9 @@ import {
 	ModalOpenButton,
 } from "../components/Modal";
 import { toast } from "react-toastify";
-import { Input } from "../components/Input";
+import Input from "../components/Input";
 import Select from "../components/Select";
+import { LinkIcon } from "../components/icons";
 
 const initialNewProductDetails = {
 	name: "",
@@ -26,6 +27,7 @@ const ViewInventory = () => {
 	const categories = useCategories();
 	const [newProduct, setNewProduct] = useState(initialNewProductDetails);
 	const [category, setCategory] = useState("All");
+	const [search, setSearch] = useState("");
 
 	const resetNewProduct = () => setNewProduct(initialNewProductDetails);
 
@@ -45,15 +47,25 @@ const ViewInventory = () => {
 		(input) => input === ""
 	);
 
-	const filteredProducts =
+	const productsFilteredByCategory =
 		category === "All"
 			? products
 			: products.filter((product) => category === product.category.name);
 
+	const productsFilteredBySearch = productsFilteredByCategory.filter(
+		(product) => product.name.includes(search)
+	);
+
 	return (
 		<div className="prose md:max-w-lg lg:max-w-4xl mx-auto">
 			<h2 className="text-center mt-10">View Inventory</h2>
-			<div className="px-1">
+			<div className="px-1 md:grid md:grid-cols-2 md:gap-2">
+				<Input
+					placeholder="Search"
+					value={search}
+					onChange={(e) => setSearch(e.target.value.toLowerCase())}
+					className="mb-3 md:mb-0"
+				/>
 				<Select
 					value={category}
 					onChange={({ target }) => setCategory(target.value)}
@@ -79,62 +91,57 @@ const ViewInventory = () => {
 					</tr>
 				</thead>
 				<tbody>
-					{filteredProducts.map((product, index) => {
-						const lastOrderedDate =
-							formatDate(product?.lastOrderedDate) ?? "--";
+					{productsFilteredBySearch
+						.sort((a, b) => {
+							return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1;
+						})
+						.map((product, index) => {
+							const lastOrderedDate =
+								formatDate(product?.lastOrderedDate) ?? "--";
 
-						const lastReceivedDate =
-							formatDate(product?.lastReceivedDate) ?? "--";
+							const lastReceivedDate =
+								formatDate(product?.lastReceivedDate) ?? "--";
 
-						return (
-							<tr key={product._id}>
-								<td>{index + 1}</td>
-								<td>
-									<ProductName product={product} />
-								</td>
-								<td>
-									<div>{`${product.quantity} ${product?.unitOfMeasure ?? ""}${
-										product.quantity > 1 && product?.unitOfMeasure ? "s" : ""
-									}`}</div>
-									{product?.unitsPerContainer ? (
-										<div className="text-sm opacity-50">
-											{`${product.unitsPerContainer} / 
+							return (
+								<tr key={product._id}>
+									<td>{index + 1}</td>
+									<td>
+										<ProductName product={product} />
+									</td>
+									<td>
+										<div>{`${product.quantity} ${product?.unitOfMeasure ?? ""}${
+											product.quantity > 1 && product?.unitOfMeasure ? "s" : ""
+										}`}</div>
+										{product?.unitsPerContainer ? (
+											<div className="text-sm opacity-50">
+												{`${product.unitsPerContainer} / 
 												${product?.unitOfMeasure ?? "Container"}`}
-										</div>
-									) : null}
-									{product.quantity < product?.lowQuantity ? (
-										<div className="badge badge-error badge-xs">Low</div>
-									) : null}
-								</td>
-								<td>{product.status === "None" ? "--" : product.status}</td>
-								<td>{lastOrderedDate}</td>
-								<td>{lastReceivedDate}</td>
-								<td>
-									{product?.purchaseLink ? (
-										<a
-											className="no-underline"
-											target="_blank"
-											rel="noreferrer"
-											href={`${product.purchaseLink}`}
-										>
-											<svg
-												width="24"
-												height="24"
-												xmlns="http://www.w3.org/2000/svg"
-												fillRule="evenodd"
-												clipRule="evenodd"
-												fill="currentColor"
+											</div>
+										) : null}
+										{product.quantity < product?.lowQuantity ? (
+											<div className="badge badge-error badge-xs">Low</div>
+										) : null}
+									</td>
+									<td>{product.status === "None" ? "--" : product.status}</td>
+									<td>{lastOrderedDate}</td>
+									<td>{lastReceivedDate}</td>
+									<td>
+										{product?.purchaseLink ? (
+											<a
+												className="no-underline"
+												target="_blank"
+												rel="noreferrer"
+												href={`${product.purchaseLink}`}
 											>
-												<path d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z" />
-											</svg>
-										</a>
-									) : (
-										"--"
-									)}
-								</td>
-							</tr>
-						);
-					})}
+												<LinkIcon />
+											</a>
+										) : (
+											"--"
+										)}
+									</td>
+								</tr>
+							);
+						})}
 				</tbody>
 			</ProductTable>
 
