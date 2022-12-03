6ac17d6d0efc4576165178ea13d2bef556d79553
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-12-02 21:34:26 -0800

    Fix bug that crashes app when reach end of list and add a height to product names in card and the actual card div to standardize the heights

--- src/pages/CountInventory.jsx
+++ src/pages/CountInventory.jsx
@@ -67,72 +67,75 @@ const CountInventory = () => {
 
 			<div className="flex flex-col items-center">
 				{isEndOfList ? (
-					<div className="card w-96 bg-neutral text-neutral-content">
+					<div className="card w-96 bg-neutral text-neutral-content h-80">
 						<div className="card-body items-center text-center">
-							<h2 className="card-title">End of Inventory Counts</h2>
-							<p className="py-3">You're all done!</p>
+							<h3 className="card-title">End of Inventory Counts!</h3>
+							<p className="py-3">
+								You're all done! Click the "Start Over" button and you'll be
+								brought to the first item in the list!
+							</p>
 							<div className="card-actions justify-end">
 								<button className="btn btn-primary" onClick={restartCounts}>
-									Start again
+									Start over
 								</button>
 							</div>
 						</div>
 					</div>
 				) : (
-					<>
-						<div className="card bg-gray-800 w-96">
-							<div className="card-body">
-								<h3 className="card-title">{product.name}</h3>
-								<Input
-									type="number"
-									label="Quantity"
-									sublabel={`Current Quantity: ${product.quantity}`}
-									value={quantity}
-									onChange={({ target }) => setQuantity(target.value)}
-								/>
-								<div className="card-actions justify-end">
-									<button
-										className={`btn btn-secondary ${
-											product.status !== "None" ? "btn-disabled" : ""
-										}`}
-										onClick={requestProduct}
-									>
-										{product.status !== "None" ? product.status : "Request"}
-									</button>
-									<button
-										className={`btn btn-success ${
-											isConfirmDisabled ? "btn-disabled" : ""
-										}`}
-										onClick={confirmQuantity}
-									>
-										Confirm
-									</button>
-								</div>
+					<div className="card bg-gray-800 w-96 h-80">
+						<div className="card-body">
+							<h3 className="card-title h-14 mt-3">{product.name}</h3>
+							<Input
+								type="number"
+								label="Quantity"
+								sublabel={`Current Quantity: ${product.quantity}`}
+								value={quantity}
+								onChange={({ target }) => setQuantity(target.value)}
+							/>
+							<div className="card-actions justify-end">
+								<button
+									className={`btn btn-secondary ${
+										product.status !== "None" ? "btn-disabled" : ""
+									}`}
+									onClick={requestProduct}
+								>
+									{product.status !== "None" ? product.status : "Request"}
+								</button>
+								<button
+									className={`btn btn-success ${
+										isConfirmDisabled ? "btn-disabled" : ""
+									}`}
+									onClick={confirmQuantity}
+								>
+									Confirm
+								</button>
 							</div>
 						</div>
-						<div className="btn-group grid grid-cols-3 w-96 mt-3">
-							<button
-								className={`btn btn-outline ${
-									isPrevousDisabled ? "btn-disabled" : ""
-								}`}
-								onClick={previousOnClick}
-							>
-								«
-							</button>
-							<button className="btn btn-outline">
-								{products.length - index} Remaining
-							</button>
-							<button
-								className={`btn btn-outline ${
-									isNextDisabled ? "btn-disabled" : ""
-								}`}
-								onClick={nextOnClick}
-							>
-								»
-							</button>
-						</div>
-					</>
+					</div>
 				)}
+			</div>
+			<div className="flex justify-center">
+				<div className="btn-group grid grid-cols-3 w-96 mt-3">
+					<button
+						className={`btn btn-outline ${
+							isPrevousDisabled ? "btn-disabled" : ""
+						}`}
+						onClick={previousOnClick}
+					>
+						«
+					</button>
+					<button className="btn btn-outline">
+						{products.length - index} Remaining
+					</button>
+					<button
+						className={`btn btn-outline ${
+							isNextDisabled ? "btn-disabled" : ""
+						}`}
+						onClick={nextOnClick}
+					>
+						»
+					</button>
+				</div>
 			</div>
 
 			<h2 className="text-center mb-0">Requesting</h2>
@@ -154,7 +157,7 @@ const CountInventory = () => {
 									<tr key={requestedProduct._id}>
 										<td>{index + 1}</td>
 										<td>
-											<ProductName product={product} />
+											<ProductName product={requestedProduct} />
 										</td>
 										<td>
 											<button
