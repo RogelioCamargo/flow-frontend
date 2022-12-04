38de241cbf755f5e2c4f7575327a5ca95ed67ced
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-12-03 22:07:25 -0800

    Install react-error-boundary dependency and wrap an ErrorBoundary component around AuthenticatedApp children to catch any ui errors

--- src/AuthenticatedApp.jsx
+++ src/AuthenticatedApp.jsx
@@ -6,16 +6,35 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import AsideMenu from "./components/AsideMenu";
 import Product from "./pages/Product";
+import { ErrorBoundary } from "react-error-boundary";
+
+function FullPageErrorFallback({ error }) {
+	return (
+		<div
+			role="alert"
+			className="h-screen w-screen flex flex-col justify-center items-center"
+		>
+			<div className="text-center w-3/4">
+				<p className="mb-3">
+					Uh oh... There's a problem. Try refreshing the app.
+				</p>
+				<pre className="break-words-and-wrap">{error.message}</pre>
+			</div>
+		</div>
+	);
+}
 
 function AuthenticatedApp() {
 	return (
-		<>
-			<Header />
-			<main>
-				<AsideMenu />
-				<AppRoutes />
-			</main>
-		</>
+		<ErrorBoundary FallbackComponent={FullPageErrorFallback}>
+			<div>
+				<Header />
+				<main>
+					<AsideMenu />
+					<AppRoutes />
+				</main>
+			</div>
+		</ErrorBoundary>
 	);
 }
 
