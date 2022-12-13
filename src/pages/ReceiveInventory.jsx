06a944b1312d2eaf7bf7350da72ffeb091500c1c
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-12-12 21:51:38 -0800

    Implement feature to receive product that hasn't been requested

--- src/pages/ReceiveInventory.jsx
+++ src/pages/ReceiveInventory.jsx
@@ -43,7 +43,7 @@ function ReceiveInventory() {
 										<ProductName product={product} />
 									</td>
 									<td>
-										<ReceiveProductModal product={product} />
+										<ReceiveSelectedProductModal product={product} />
 									</td>
 								</tr>
 							);
@@ -51,11 +51,90 @@ function ReceiveInventory() {
 					</tbody>
 				</ProductTable>
 			)}
+
+			<ReceiveProductModal products={products} />
 		</div>
 	);
 }
 
-function ReceiveProductModal({ product }) {
+function ReceiveProductModal({ products }) {
+	const [search, setSearch] = useState("");
+	const [quantity, setQuantity] = useState("");
+	const { mutate: updateProduct } = useUpdateProduct();
+	const [selectedProduct, setSelectedProduct] = useState(null);
+
+	const results = products.filter((product) =>
+		product.name.toLowerCase().includes(search.toLocaleLowerCase())
+	);
+
+	const confirmQuantity = () => {
+		const updates = {
+			...selectedProduct,
+			quantity: selectedProduct.quantity + Number(quantity),
+			status: "None",
+			lastReceivedDate: new Date(),
+		};
+
+		updateProduct(updates);
+		toast.success("Product Received", {
+			position: "bottom-center",
+			theme: "colored",
+		});
+		setQuantity("");
+	};
+
+	return (
+		<Modal>
+			<ModalOpenButton>
+				<button className="btn btn-primary fixed bottom-5 right-5 text-3xl flex justify-center items-center text-white rounded-full w-12 h-12">
+					+
+				</button>
+			</ModalOpenButton>
+			<ModalContent title="Receive">
+				<ModalDismissButton onClick={() => setSearch("")} />
+				<Input
+					placeholder="i.e. Poly Bags"
+					label="Name"
+					value={search}
+					onChange={(e) => setSearch(e.target.value.toLowerCase())}
+					className="mb-3 md:mb-0"
+				/>
+				{search === "" ? null : results.length === 0 ? (
+					<p className="text-center">No results found</p>
+				) : !selectedProduct ? (
+					<ul className="list-none p-0 my-0 text-base overflow-y-scroll max-h-60">
+						{results.map((product) => (
+							<li
+								key={product._id}
+								className="hover:bg-primary py-2 hover:text-white hover:rounded-md"
+								onClick={() => {
+									setSearch(product.name);
+									setSelectedProduct(product);
+								}}
+							>
+								{product.name}
+							</li>
+						))}
+					</ul>
+				) : null}
+				<Input
+					type="number"
+					value={quantity}
+					label="Quantity"
+					onChange={({ target }) => setQuantity(target.value)}
+				/>
+				<ModalConfirmButton
+					className={search === "" || quantity === "" ? "btn-disabled" : ""}
+					onClick={confirmQuantity}
+				>
+					Confirm
+				</ModalConfirmButton>
+			</ModalContent>
+		</Modal>
+	);
+}
+
+function ReceiveSelectedProductModal({ product }) {
 	const { mutate: updateProduct } = useUpdateProduct();
 	const [quantity, setQuantity] = useState("");
 
